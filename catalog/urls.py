dde0d207081e50d59977b4cf9ff3e8c75059a7cd
CameronGuthrie <cguthrie@qa.com>
2023-04-23 00:15:52 +0100

    tutorial 1 complete

---
+++ catalog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('product_list/', views.ProductListView.as_view(), name='product_list'),
+    path('product_detail/<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
+    path('category_list/', views.CategoryListView.as_view(), name='category_list'),
+    path('category_detail/<int:pk>', views.CategoryDetailView.as_view(), name='category_detail'),
+]
